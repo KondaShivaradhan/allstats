fbe08a3195d62f55b974611ce10a36e22dfd0792
Konda Shivaradhan <38292566+KondaShivaradhan@users.noreply.github.com>
2021-05-02 00:40:19 +0530

    Battlefield , Error
    Battlefield basic,
    Error Handling for all games,
    Router error not fixed

--- app.js
+++ app.js
@@ -26,18 +26,7 @@ app.get('/', function(req, res) {
 app.get('/battlefield', function(req, res) {
     res.render('bfs')
 });
-app.get('/BlazingBaneApex', function(req, res) {
-    apex.user('BlazingBane', 'PC').then(data => {
-        var aap = data.data
-        global.ap = aap
-        if (typeof ap != 'undefined')
-            res.render('apexold', { ap });
-        else {
-            res.render('refresh')
 
-        }
-    });
-});
 app.get('/bf/:Name', function(req, res) {
     var bfv = 0
     var bf1 = 0
@@ -45,37 +34,28 @@ app.get('/bf/:Name', function(req, res) {
     var nobf = 0
     var id = req.params.Name
     const start = async function() {
-        let response5
-        response5 = await axios.get(
-            "https://api.gametools.network/bfv/stats/?name=" + id + "&lang=en-us", {}
-        ).catch(error => {
-            console.log(error);
-            bfv = undefined
-        });
-        if (response5.data.error) {
-            bfv = undefined
-        }
-        let response6
-        response6 = await axios.get(
-            "https://api.gametools.network/bfv/weapons/?name=" + id + "&lang=en-us", {}
-        ).catch(error => {
-            bfv = undefined
-        });
-        if (response5.data.error) {
-            bfv = undefined
-        }
         let response4
         response4 = await axios.get(
             "https://api.gametools.network/bf4/stats/?name=" + id + "&lang=en-us", {}
         ).catch(error => {
-            bf4 = 'No bf4'
-        });
-        if (response4.data.error) {
             bf4 = undefined
+        });
+        let response5
+        response5 = await axios.get(
+            "https://api.gametools.network/bfv/all/?name=" + id + "&lang=en-us", {}
+        ).catch(error => {
+            bfv = undefined
+        });
+        if (response5.data.error) {
+            bfv = undefined
         }
+
+        // if (response4.data.error) {
+        //     bf4 = undefined
+        // }
         let response1
         response1 = await axios.get(
-            "https://api.gametools.network/bf1/stats/?name=" + id + "&lang=en-us", {}
+            "https://api.gametools.network/bf1/all/?name=" + id + "&lang=en-us", {}
         ).catch(error => {
             bf1 = undefined
         });
@@ -85,10 +65,14 @@ app.get('/bf/:Name', function(req, res) {
         if (bf1 != undefined)
             bf1 = response1.data
         if (bf4 != undefined)
-            bf4 = response4.data
+            bf4 = undefined
+            // bf4 = response4.data
+
         if (bfv != undefined)
-            bfv = {...response5.data, ...response6.data }
+            bfv = {...response5.data }
         console.log(bf1);
+        console.log(bfv);
+
         if (bf1 == undefined && bfv == undefined && bf4 == undefined) {
             nobf = 'Buy a Game NOOB'
         }
@@ -97,70 +81,46 @@ app.get('/bf/:Name', function(req, res) {
     }
     start()
 });
-app.get('/bfv/:Name', function(req, res) {
-    var bfv = 0
 
-    var nobf = 0
-    var id = req.params.Name
-    const start = async function() {
-        let response5
-        response5 = await axios.get(
-            "https://api.gametools.network/bfv/stats/?name=" + id + "&lang=en-us", {}
-        ).catch(error => {
-            console.log(error);
-            bfv = undefined
-        });
-        if (response5.data.error) {
-            bfv = undefined
-        }
-        let response6
-        response6 = await axios.get(
-            "https://api.gametools.network/bfv/weapons/?name=" + id + "&lang=en-us", {}
-        ).catch(error => {
-            bfv = undefined
-        });
-        if (response5.data.error) {
-            bfv = undefined
-        }
-        if (bfv != undefined)
-            bfv = {...response5.data, ...response6.data }
-        console.log(bfv);
-        if (bf1 == undefined && bfv == undefined && bf4 == undefined) {
-            nobf = 'Buy a Game NOOB'
-        }
-        res.render('bf', { bfv, id, nobf })
-
-    }
-    start()
-});
 app.get('/apex/:Name', function(req, res) {
+
     const id = req.params.Name
     apex.user(id, 'PC').then(data => {
         global.ap = {...data.data }
-
+        console.log(ap);
         var obj = {};
-        ap.stats.forEach(element => {
-            Object.assign(obj, {
-                [element.metadata.name]: element.value
+        try {
+            ap.stats.forEach(element => {
+                Object.assign(obj, {
+                    [element.metadata.name]: element.value
+                });
             });
-        });
+            if (typeof ap != 'undefined')
+                res.render('apexold', { ap, obj });
+            else {
+                res.render('refresh')
 
-        if (typeof ap != 'undefined')
-            res.render('apexold', { ap, obj });
-        else {
+            }
+        } catch (error) {
             res.render('refresh')
-
         }
     });
 });
+
 app.get('/r6/:Name', function(req, res) {
     const un = req.params.Name
     const username = req.params.Name
     platform = 'uplay';
     const start = async function() {
-        const id = await r6api.getId(platform, username).then(el => el[0].userId);
+        try {
+            const id = await r6api.getId(platform, username).then(el => el[0].userId);
+        } catch (error) {
+            res.render('refresh')
+        }
+
         const stats = await r6api.getStats(platform, id).then(el => el[0]);
         const rank = await r6api.getRank('uplay', id, { regions: ['apac'] });
+
         ba = rank[0]
         ra = rank[0].seasons[Object.keys(ba.seasons)].regions.apac
         var obj = {};
@@ -210,44 +170,7 @@ app.get('/r6/:Name', function(req, res) {
 app.get('/pc', function(req, res) {
     res.render('pc')
 });
-app.get('/BlazingBaneR6', function(req, res) {
-    const username = 'BlazingBane'
-    platform = 'uplay';
-    const start = async function() {
-        const id = await r6api.getId(platform, username).then(el => el[0].userId);
-        const stats = await r6api.getStats(platform, id).then(el => el[0]);
-        const rank = await r6api.getRank('uplay', id);
-        ba = rank[0]
-        ra = rank[0].seasons[Object.keys(ba.seasons)].regions.apac
-        await r6.getGenericStats('BlazingBane', 'pc', 'all').then(userStats => {
-            rdata = userStats
-            global.r = rdata
-        })
-        await r6.getOperatorStats('BlazingBane', 'pc').then(userStats => {
-            var rdata = userStats
-            global.r1 = rdata
-            if (typeof r != 'undefined' && typeof r1 != 'undefined') {
-                var dmax = 1;
-                var amax = 1;
-                r1.operators.forEach(element => {
-                    if (dmax < element.kills && element.role == 'Defender') {
-                        dmax = element.kills
-                    }
-                })
-                r1.operators.forEach(element => {
 
-                    if (amax < element.kills && element.role == 'Attacker') {
-                        amax = element.kills
-                    }
-                })
-                res.render('r6', { r, r1, dmax, amax, ra });
-            } else {
-                res.render('refresh')
-            }
-        })
-    }
-    start()
-});
 app.get('/discord', function(req, res) {
     res.render('discord')
 });
